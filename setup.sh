cf96f2745293e5e95b66005488a0878a80d9327c
Umut Yalcinkaya <52127658+87percentrum@users.noreply.github.com>
2021-08-20 20:18:46 +1000

    added sftp support

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cp -R /root/.ssh /home/$NEWUSER
 
 chown -R $NEWUSER:$NEWUSER /home/$NEWUSER/.ssh
 
-printf "Port 1982\nPasswordAuthentication no\nPubkeyAuthentication yes\nAcceptEnv LANG LC_*\nPrintMotd no\nX11Forwarding yes\nUsePAM yes\nPermitRootLogin yes\nPermitRootLogin yes%s" "" > /etc/ssh/sshd_config
+printf "Port 1982\nPasswordAuthentication no\nPubkeyAuthentication yes\nAcceptEnv LANG LC_*\nPrintMotd no\nX11Forwarding yes\nUsePAM yes\nPermitRootLogin yes\nPermitRootLogin yes\nSubsystem       sftp    /usr/lib/openssh/sftp-server\nMatch group sftp\nX11Forwarding no\nAllowTCPForwarding no\nForceCommand internal-sftp%s" "" > /etc/ssh/sshd_config
 service ssh restart
 
 printf "\n%s ALL=(ALL) NOPASSWD:ALL" $NEWUSER >> /etc/sudoers
